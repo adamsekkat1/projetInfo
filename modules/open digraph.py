cfecb162311555198ca20e1e3c6c1ee0908c6551
Mac <mac@MacBook-Air-de-Mac.local>
2022-01-18 14:12:04 +0100

    a

--- modules/open digraph.py
+++ modules/open digraph.py
@@ -1,1 +1,22 @@
-open digraph.py
+class node:
+	def __init__(self, identity, label, parents, children):
+		'''
+		identity: int; its unique id in the graph
+		label: string;
+		parents: int->int dict; maps a parent node's id to its multiplicity
+		children: int->int dict; maps a child node's id to its multiplicity
+		'''
+		self.id = identity
+		self.label = label
+		self.parents = parents
+		self.children = children
+class open_digraph: # for open directed graph
+	def __init__(self, inputs, outputs, nodes):
+		'''
+		inputs: int list; the ids of the input nodes
+		outputs: int list; the ids of the output nodes
+		nodes: node iter;
+		'''
+		self.inputs = inputs
+		self.outputs = outputs
+		self.nodes = {node.id:node for node in nodes} # self.nodes: <int,node> dict
