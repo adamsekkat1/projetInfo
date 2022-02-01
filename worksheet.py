51ed090a92643629d1f332b7d0c0e676964e23d3
Victor Robert <victor@MacBook-Air-de-Victor.local>
2022-02-01 14:20:47 +0100

    tp2

--- worksheet.py
+++ worksheet.py
@@ -2,14 +2,15 @@ from modules.open_digraph import *
 import inspect 
 
 
-#a=open_digraph.empty() 
-<<<<<<< HEAD
+a=open_digraph.empty() 
+a.add_node()
+a.add_node()
 #a=open_digraph([1,2],[3,4],[node(1,"",[],[2]),node(2,"",[1],[])])
-#print(a)
+print(a)
 #print(a.new_id(9))
-=======
-a=open_digraph([1,2],[3,4],[node(1,"",[],[2]),node(2,"",[1],[])])
->>>>>>> 6b24498d5e76e5e8e87680a84fbc017a005703b3
+
+#a=open_digraph([1,2],[3,4],[node(1,"",[],[2]),node(2,"",[1],[])])
+
 #print(dir(open_digraph))
 #affiche code source :
 #l=inspect.getsource(open_digraph.copy)
