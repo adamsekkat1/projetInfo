24853756b80f66bd05274c6b376920d53f6761dd
Manda Andriamaromanana <aroniaina.andriam@gmail.com>
2022-02-22 15:36:16 +0100

    td6 suite

--- modules/open_digraph.py
+++ modules/open_digraph.py
@@ -605,14 +605,32 @@ class open_digraph:  # for open directed graph
         self.inputs = [ids+n for ids in self.inputs]
         self.outputs = [ids+n for ids in self.outputs]
 
+    '''
     def iparallel(self,g):
         self.shift_indices(g.max_id())
         self.nodes.update(g.nodes)
+        self.inputs += g.inputs
+        self.outputs += g.outputs
 
     def parallel(self,g):
         j = self.copy()
         j.iparallel(g)
         return j
+    '''
+    
+
+    def iparallel_l(self,l):
+        for g in l:
+            self.shift_indices(g.max_id())
+            self.nodes.update(g.nodes)
+            self.inputs += g.inputs
+            self.outputs += g.outputs
+
+    def parallel_l(self,l):
+        j = self.copy()
+        j.iparallel_l(l)
+        return j
+    
 
     def icompose(self,g):
         if len(self.outputs) != len(g.inputs):
@@ -629,6 +647,50 @@ class open_digraph:  # for open directed graph
         j = self.copy()
         j.icompose(g)
         return j
+
+    def id_in_components(self,idn,components):
+        for l in components:
+            if idn in l:
+                return l
+        return None
+
+    def connected_components(self):
+        components = []
+        for idn,n in self.nodes:
+            l = self.id_in_components(idn,components)
+            if l is None:
+                l = []
+                l.append(idn)
+                l+= n.get_children_ids()
+                l+= n.get_parents_ids()
+                components.append(l)
+            else:
+                for child in idn.get_children_ids():
+                    if child not in l:
+                        l.append(child)
+                for parent in idn.get_parents_ids():
+                    if parent not in l:
+                        l.append(parent)
+        dic = {}
+        for idn,n in self.nodes:
+            dic[idn] = components.index(self.id_in_components(idn,components))
+        return len(components), dic
+
+    def get_open_digraphs(self):
+        num, dic = self.connected_components()
+        l = []
+        for n in range(num):
+            g = open_digraph.empty()
+            for idn,val in dic.keys():
+                if val== n:
+                    g.nodes[idn] = self.get_node_by_id(idn)
+                    if idn in self.inputs:
+                        g.inputs.append(idn)
+                    if idn in self.outputs:
+                        g.outputs.append(idn)
+            l.append(g)
+        return l
+
 
 def is_cyclic_aux(graph):
     if graph.nodes == {}:
